91d89c62b89d98a6ef1ff8929fa8ccb93716094e
allxdamnxday <braden.j.freeman88@gmail.com>
2025-03-20 18:35:04 -0700

    Update favicon and page title

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import Logo from './components/Logo';
 import Flyer from './components/Flyer';
 import RSVPButton from './components/RSVPButton';
 import EventInfo from './components/EventInfo';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="relative h-screen overflow-hidden">
+      <Helmet>
+        <title>LEiNK</title>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="alternate icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <meta name="theme-color" content="#000000" />
+      </Helmet>
       <Background />
       <div className="absolute inset-0 bg-black bg-opacity-50 z-[1]"></div>
       
